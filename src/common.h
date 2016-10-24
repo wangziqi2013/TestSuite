63a30a2d4c8721de988313967f604d5625b66318
= <675973885@qq.com>
2016-10-24 19:14:15 -0400

    Adding common.h

---
+++ src/common.h
@@ -0,0 +1,43 @@
+
+#pragma once
+
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#include <cassert>
+#include <thread>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <unordered_map>
+#include <atomic>
+#include <thread>
+#include <cstdint> 
+
+#define DEBUG_PRINT
+
+#ifdef DEBUG_PRINT
+
+#define dbg_printf(fmt, ...)                              \
+  do {                                                    \
+    fprintf(stderr, "%-24s: " fmt, __FUNCTION__, ##__VA_ARGS__); \
+    fflush(stdout);                                       \
+  } while (0);
+
+#else
+
+static void dummy(const char*, ...) {}
+
+#define dbg_printf(fmt, ...)   \
+  do {                         \
+    dummy(fmt, ##__VA_ARGS__); \
+  } while (0);
+
+#endif
+
+// I copied this from Linux kernel code to favor branch prediction unit on CPU
+// if there is one
+#define likely(x)   __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+ 
+#endif
