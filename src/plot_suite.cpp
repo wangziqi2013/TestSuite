f181ac0d67fd3468aff8eabbaa9bf50e95fa64a8
Ziqi Wang <675973885@qq.com>
2017-02-21 22:54:47 -0500

    adding cpp file for holding global objects

---
+++ src/plot_suite.cpp
@@ -0,0 +1,15 @@
+
+#include "plot_suite.h"
+
+extern ChartParameter default_chart_param{
+  // Width 
+  8,   
+  // Height
+  5,   
+  // X tick font size
+  16,  
+  // y tick font size
+  14,  
+  // Legend position
+  "upper left"
+};
