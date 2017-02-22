ddcc676e3dc532a5e14efaa5e967caa1f103d21e
Ziqi Wang <675973885@qq.com>
2017-02-21 20:58:34 -0500

    Adding class Color

--- src/plot_suite.h
+++ src/plot_suite.h
@@ -8,6 +8,59 @@
 #include "test_suite.h"
 
 /*
+ * class Color - RGB color scheme
+ */
+class Color {
+ public:
+  uint8_t b; 
+  uint8_t g;
+  uint8_t r;
+  
+  // This is used as a padding and is not used
+  uint8_t zero;
+  
+  /*
+   * Constructor
+   */
+  Color() :
+    b{0},
+    g{0},
+    r{0},
+    zero{0}
+  {}
+  
+  /*
+   * Constructor
+   *
+   * Please note that although the internal data is stored using BGR order
+   * we still pass arguments using RGB order
+   */
+  Color(uint8_t p_r, uint8_t p_g, uint8_t p_b) :
+    b{p_b},
+    g{p_g},
+    r{p_r},
+    zero{0}
+  {}
+  
+  /*
+   * AppendToBuffer() - Appends the value of the color to the buffer
+   *
+   * The color will be appended like this: #RRGGBB where RR GG BB are 8 bit
+   * color values in hex
+   */
+  void AppendToBuffer(Buffer *buffer_p) {
+    buffer_p->Append('#');
+    
+    // Then Append RGB respectively
+    buffer_p->Printf("%02X", static_cast<uint32_t>(r));
+    buffer_p->Printf("%02X", static_cast<uint32_t>(g));
+    buffer_p->Printf("%02X", static_cast<uint32_t>(b));
+    
+    return;
+  }
+};
+
+/*
  * class BarGroup - This class represents a group in the graph
  */
 class BarGroup {
@@ -74,6 +127,29 @@ class BarGroup {
     
     return;
   }
+};
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+/*
+ * class BarChart - Represents a bar chart, including its data and metadata
+ *                  such as graph title,  
+ */
+class BarChart {
+ private:
+  // This stores data and group title.
+  // Note that it is not required that all group have the same length
+  // However, when drawing the diagram, we will calculate the maximum
+  // number of elements among all groups, and use this value to determine
+  // the width of each group 
+  std::vector<BarGroup> group_list;
+  
+  // The title of the bar chart. If its length is 0 we do not draw anything 
+  std::string chart_title;
+  
+  
 };
 
 #endif
